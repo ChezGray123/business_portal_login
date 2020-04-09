538c5cf53c2cd4eda381a246e2df036fdf78300c
ChezGray123 <63345723+ChezGray123@users.noreply.github.com>
2020-04-09 10:10:40 +0100

    Add files via upload

---
+++ Intranet_login.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <title>Staff Portal Login</title>
+        <link rel="stylesheet" type="text/css"href="style.css">
+    <head>
+    <body>
+    <div class="main">
+        <!--Left Pannel-->
+        <div class="left_col">
+            <div class="left_content">
+                <h1>Empower your team. Lead the industry.</h1>
+                <div class="login-left-text">
+                    <p>
+                        Introduce your business and the products/services you are offering users. 
+                        Provide viewers with enough information to entice them to click the find 
+                        out more button.
+                    </p>
+                </div>
+                <a class="btn" href="">Find Out More</a>
+            </div>
+        </div>
+        <!--Right Pannel-->
+        <div class="right_col">
+            <div class="login_container">
+                <div class="container_header">
+                    <h2>Your Staff Portal</h2>
+                    <p>Welcome Back! Please Login Below.<p>
+                </div>
+                <div class="login_user_details">
+                    <div class="user_name">
+                        <label for="username">Username</label>
+                        <input name="username" type="text" placeholder="username">
+                    </div>
+                    <div class="password">
+                        <label for="password">Password</label>
+                        <input name="password" type="password" placeholder="********">
+                    </div> 
+                </div>
+                <div class="login_buttons_container">
+                    <button class="btn btn-reset">Reset Password</button>
+                    <button class="btn btn-login">Login</button>
+                </div>
+            </div>
+            <div class="right_col_footer">
+                <p>&#169;Example Company 2020<p>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
